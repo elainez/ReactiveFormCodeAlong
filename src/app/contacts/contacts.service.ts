1c35d7a19142a48b1a9b8d904e899451d2940d29
elaine zhou <elaineyzhou28@gmail.com>
2025-06-19 12:20:14 +1200

    Initial commit

---
+++ src/app/contacts/contacts.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Contact } from './contact.model';
+import { nanoid } from 'nanoid'
+/*
+nanoid is a tiny, secure, URL-friendly, and fast unique ID generator often used in JavaScript and Angular projects to:
+
+Generate unique keys or IDs for components
+
+Assign IDs to items (like in a shopping cart or form)
+
+Avoid collisions when manually assigning identifiers
+
+It’s an alternative to UUIDs — but shorter and faster.
+const id = nanoid(5);
+The 5 is the length of the generated ID
+
+The characters include letters (A-Z, a-z) and numbers (0–9)
+*/ 
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactsService {
+  constructor(private http: HttpClient) { }
+
+  getContact(id: string): Observable<Contact | undefined> {
+    return this.http.get<Contact>(`api/contacts/${id}`)
+      .pipe(map(c => {
+        const dob = c.dateOfBirth ? new Date(c.dateOfBirth) : null;
+        return { ...c, dateOfBirth: dob }
+      }));
+  }
+
+  getAllContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('api/contacts');
+  }
+
+  saveContact(contact: Contact): Observable<Contact> {
+    const headers = { headers: { 'Content-Type': 'application/json' } };
+
+    if (!contact.id || contact.id === '') {
+      let newContact: Contact = { ...contact, id: nanoid(5) };
+      return this.http.post<Contact>('api/contacts/', newContact, headers)
+    }
+    else
+      return this.http.put<Contact>('api/contacts/', contact, headers)
+  }
+}
